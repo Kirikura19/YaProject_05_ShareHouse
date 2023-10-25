2cc8278746a9384a031a14fb696fcc483e3fea91
Kirikura <kirikura.w@yandex.ru>
2023-10-25 21:29:02 +0300

    Init commit

---
+++ src/main/java/ru/kirikura/yaproject_05_sharehouse/YaProject05ShareHouseApplication.java
@@ -0,0 +1,13 @@
+package ru.kirikura.yaproject_05_sharehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaProject05ShareHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YaProject05ShareHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kirikura/yaproject_05_sharehouse/YaProject05ShareHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kirikura.yaproject_05_sharehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaProject05ShareHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
